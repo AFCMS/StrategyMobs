08293d97ee4067a2f66d396ab040ddbd571a61ed
AFCMS <afcm.contact@gmail.com>
2023-12-21 11:43:15 +0100

    WIP `stent` entrance formspec

--- mods/strategymobs_init/init.lua
+++ mods/strategymobs_init/init.lua
@@ -2,6 +2,107 @@ minetest.log("action", "[strategymobs_init] Loading...")
 
 stent.start_location = vector.new(0, 1, 0)
 
+local player_menu_data = {}
+
+---@param p_name string
+---@param arenas_data stent.arena_data[]
+---@return table
+local function fs_tree_creator(p_name, arenas_data)
+	local listelems = {}
+	local menu_data = player_menu_data[p_name] or {}
+	for i, arena_data in ipairs(arenas_data) do
+		local str = "Waiting"
+		if arena_data.in_queue then
+			str = "Queueing"
+		end
+		if arena_data.in_loading then
+			str = "Loading"
+		end
+		if arena_data.in_game then
+			str = "In Progress"
+		end
+		if arena_data.in_celebration then
+			str = "Finishing"
+		end
+		if arena_data.enabled == false then
+			str = "Disabled"
+		end
+		local entry = arena_data.name ..
+			"     " .. arena_data.players_inside .. "/" .. arena_data.max_players .. "     " .. str
+		table.insert(listelems, entry)
+	end
+	local tree = {
+		{ type = "size",   w = 10.5, h = 11, fixed_size = false },
+		---{ type = "image",  x = 0,    y = 0,  w = 10.5,          h = 2.6, texture_name = "header.png" },
+		{ type = "button", x = 0,    y = 3,  w = 10.5,          h = 0.8, name = "btn1", label = "Available Arenas" },
+		{
+			type = "textlist",
+			x = 0,
+			y = 3.8,
+			w = 10.5,
+			h = 3,
+			name = "arena_list_example",
+			listelems = listelems,
+			selected_idx = menu_data.selected_idx or 1,
+			transparent = true
+		},
+		{ type = "button", x = .3,  y = 7.2, w = 3.1, h = 0.8, name = "join",     label = "Join Queue" },
+		{ type = "button", x = 3.7, y = 7.2, w = 3.3, h = 0.8, name = "leave",    label = "Leave Queue" },
+		{ type = "button", x = 7.1, y = 7.2, w = 3.1, h = 0.8, name = "spectate", label = "Spectate" },
+	}
+	return tree
+end
+
+---@param p_name string
+---@param arenas_data stent.arena_data[]
+---@diagnostic disable-next-line: duplicate-set-field
+function stent.build_mainmenu_formspec(p_name, arenas_data)
+	local tree = fs_tree_creator(p_name, arenas_data)
+	return formspec_ast.interpret(tree)
+end
+
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+	if formname ~= "" and formname ~= "main_menu" then return end
+
+	local p_name = player:get_player_name()
+
+	if fields.arena_list_example then
+		local evt = minetest.explode_textlist_event(fields.arena_list_example)
+		if evt.type == "CHG" then
+			player_menu_data[p_name] = player_menu_data[p_name] or {}
+			player_menu_data[p_name].selected_idx = evt.index
+		end
+	end
+
+	if fields.join then
+		if player_menu_data[p_name] and player_menu_data[p_name].selected_idx then
+			local data = stent.saved_arenas_data[player_menu_data[p_name].selected_idx]
+			local arena_id, arena = arena_lib.get_arena_by_name(data.mod, data.name)
+			if arena.in_game then
+				arena_lib.join_arena(data.mod, p_name, arena_id)
+			else
+				arena_lib.join_queue(data.mod, arena, p_name)
+			end
+		end
+	end
+
+	if fields.leave then
+		arena_lib.remove_player_from_queue(p_name)
+	end
+
+	if fields.spectate then
+		if player_menu_data[p_name] and player_menu_data[p_name].selected_idx then
+			local data = stent.saved_arenas_data[player_menu_data[p_name].selected_idx]
+			local arena_id, arena = arena_lib.get_arena_by_name(data.mod, data.name)
+			local modref = arena_lib.mods[data.mod]
+
+			if arena.in_game and modref.spectate_mode then
+				arena_lib.join_arena(data.mod, p_name, arena_id, true)
+			end
+		end
+	end
+end)
+
 minetest.register_on_mods_loaded(function()
 	minetest.log("action", "[strategymobs_init] Loading arena 1")
 
